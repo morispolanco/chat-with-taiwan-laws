b84fcb1723ccedece6f0df678ef30b51d10b0507
morispolanco <morispolanco@gmail.com>
2023-05-21 00:11:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-# Environment variables
+# Variables de entorno
 OPENAI_MODEL = os.environ.get("OPENAI_MODEL", "gpt-3.5-turbo")
 
-# Load embeddings and database
+# Cargar embeddings y base de datos
 embeddings = OpenAIEmbeddings()
 db = FAISS.load_local("faiss_index", embeddings)
 
-# Create a retrieval chain with the ChatOpenAI model
+# Crear una cadena de recuperación con el modelo ChatOpenAI
 chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(model_name=OPENAI_MODEL, temperature=0), chain_type="stuff", retriever=db.as_retriever(), max_tokens_limit=3500, reduce_k_below_max_tokens=True)
 
-# Streamlit input field and text area
-st.title("台灣法規問答 AI")
+# Campo de entrada y área de texto de Streamlit
+st.title("Preguntas y respuestas sobre leyes de Taiwán")
 st.markdown("""
 [![](https://img.shields.io/badge/tpai/chat_with_taiwan_laws-grey?style=flat-square&logo=github)](https://github.com/tpai/chat-with-taiwan-laws)
 """)
-# Description block
+# Bloque de descripción
 st.markdown("""
-本工具引用自全國法規資料庫之[民法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=B0000001)、[民事訴訟法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=B0010001)、[民事訴訟法施行法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=B0010002)、[中華民國刑法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=C0000001)、[中華民國刑法施行法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=C0000002)、[刑事訴訟法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=C0010001)、[刑事訴訟法施行法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=C0010002)、[社會秩序維護法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080067)、[違反社會秩序維護法案件處理辦法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080070)、[地方法院與警察機關處理違反社會秩序維護法案件聯繫辦法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080076)、[跟蹤騷擾防制法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080211)、[跟蹤騷擾防制法施行細則](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080213)、[跟蹤騷擾案件保護令執行辦法](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=D0080214)、[勞動基準法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=N0030001)、[勞工退休金條例](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=N0030020)以及[職業安全衛生設施條例](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=N0060009)之 PDF 檔案，本工具僅供研究和學習使用，如有法律需求請諮詢專業律師。
+Esta herramienta utiliza la [Base de Datos Nacional de Leyes de Taiwán](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=B0000001) que incluye las leyes civiles, la ley de procedimiento civil, la ley de ejecución del procedimiento civil, el código penal de la República de China, la ley de ejecución del código penal, la ley de procedimiento penal, la ley de ejecución del procedimiento penal, la ley de mantenimiento del orden social, el reglamento de tratamiento de casos de violación de la ley de mantenimiento del orden social, el reglamento de contacto entre tribunales locales y agencias de policía en el tratamiento de casos de violación de la ley de mantenimiento del orden social, la ley de prevención del acoso y hostigamiento, el reglamento de ejecución de la ley de prevención del acoso y hostigamiento, y el reglamento de ejecución de la orden de protección en casos de acoso y hostigamiento. Estos archivos son en formato PDF. Esta herramienta es solo para fines de investigación y aprendizaje. Si necesita asesoramiento legal, consulte a un abogado profesional.
 """)
-question = st.text_input("請輸入您的問題：")
+question = st.text_input("Por favor, ingrese su pregunta:")
 if question:
-    with st.spinner("🤖 思考中，請稍候..."):
-        output = chain({"question": f"{question} 請用台灣繁體中文回答"}, return_only_outputs=True)
-    st.text_area("🤖：", value=output["answer"], height=200)
+    with st.spinner("🤖 Pensando, por favor espera..."):
+        output = chain({"question": f"{question} Por favor, responda en chino tradicional de Taiwán"}, return_only_outputs=True)
+    st.text_area("🤖:", value=output["answer"], height=200)
